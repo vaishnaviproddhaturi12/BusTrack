b88b7cb76fd0f812ecdd091d598b7ed2832c5bcb
vaishnaviproddhaturi12 <vaishnaviproddhaturi@gmail.com>
2026-04-24 20:02:18 +0530

    Update apiConfig.js

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -1,5 +1,3 @@
-const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL ||
-  `${window.location.protocol}//${window.location.hostname}:5000`;
+const API_BASE_URL = "https://bustrack-backend-dod9.onrender.com";
 
 export default API_BASE_URL;
